49adcf91a90cc4920e8ac4e496abf0ab87987e4c
Jose Salgado <Josalgado03@gmail.com>
2019-10-31 01:46:17 -0700

    All tests are coded. CourseTests continue to abort and will not run. Have been running into this error for days and cannot figure out solution.

--- BusinessLogic/Course.cs
+++ BusinessLogic/Course.cs
@@ -6,6 +6,11 @@ namespace BusinessLogic
 {
     public class Course
     {
+        public Course()
+        {
+            CourseName = "CPW 215";
+        }
+
         public Course(string courseName)
         {
             CourseName = courseName;
@@ -14,18 +19,59 @@ namespace BusinessLogic
         /// <summary>
         /// The instructor teaching the course
         /// </summary>
-        public string InstructorName { get; set; }
+        public string InstructorName
+        {
+            get
+            {
+                return InstructorName;
+            }
+            set
+            {
+                Course c = new Course();
+            }
+        }
 
         /// <summary>
         /// The name of the course
         /// </summary>
-        public string CourseName { get; set; }
+        public string CourseName
+        {
+            get
+            {
+                return CourseName;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                else
+                    CourseName = value;
+            }
+        }
 
         /// <summary>
         /// Number of credits for the course
         /// </summary>
-        public byte NumberOfCredits { get; set; }
+        public byte NumberOfCredits
+        {
+            get
+            {
+                return NumberOfCredits;
+            }
 
+            set
+            {
+                if (value > 30 || value < 0)
+                {
+                    throw new ArgumentException
+                        ("Not proper amount of credits");
+                }
+                    NumberOfCredits = value;
+            }
+        }
         
     }
 }
